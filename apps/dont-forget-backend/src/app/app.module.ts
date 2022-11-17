d02b82b9b5eb4d53b64f31a0f3645d0ad79f6d89
Jaron van Well <jaronvwell@gmail.com>
2022-11-17 13:17:57 +0100

    trying to fix cloudflare pages build error

--- apps/dont-forget-backend/src/app/app.module.ts
+++ apps/dont-forget-backend/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
 import { NotesModule } from './notes/notes.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.DATABASE_URL),
-    NotesModule
+    NotesModule,
   ],
   controllers: [AppController],
   providers: [AppService],
