03d2522b5d06c4a29dd08e0830b26ab6542638ec
Jaron <jaronvwell@gmail.com>
2023-03-21 21:23:40 +0100

    custom validator  select W.O.P (probably obselete)

---
+++ apps/dont-forget/src/app/shared/select-user.directive.ts
@@ -0,0 +1,11 @@
+import { ValidatorFn, AbstractControl, ValidationErrors } from '@angular/forms';
+
+export function selectUserValidator(username: string): ValidatorFn {
+  return (control: AbstractControl): ValidationErrors | null => {
+    const forbidden = username == control.value;
+
+    const res = forbidden ? { forbiddenName: { value: control.value } } : null;
+    console.log(res);
+    return res;
+  };
+}
