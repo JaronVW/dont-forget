816bf06121156a69a5843415d79b8cd9b47af0b0
Jaron <jaronvwell@gmail.com>
2023-03-25 17:11:45 +0100

    auth service in progress need neo mock

--- apps/dont-forget-backend/src/app/auth/auth.service.spec.ts
+++ apps/dont-forget-backend/src/app/auth/auth.service.spec.ts
@@ -1,18 +1,70 @@
-import { Test, TestingModule } from '@nestjs/testing';
+import { Test } from '@nestjs/testing';
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import { MongoClient } from 'mongodb';
+import { Model, disconnect } from 'mongoose';
+import { User, UserSchema } from '../schemas/user.schema';
+import { MongooseModule, getModelToken } from '@nestjs/mongoose';
 import { AuthService } from './auth.service';
 
-describe('AuthService', () => {
+describe.skip('AuthService', () => {
   let service: AuthService;
+  let mongod: MongoMemoryServer;
+  let mongoc: MongoClient;
+  let userModel: Model<User>;
+  let user1;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
+  beforeAll(async () => {
+    let uri: string;
+    const app = await Test.createTestingModule({
+      imports: [
+        MongooseModule.forRootAsync({
+          useFactory: async () => {
+            mongod = await MongoMemoryServer.create();
+            uri = mongod.getUri();
+            return { uri };
+          },
+        }),
+
+        MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+      ],
       providers: [AuthService],
     }).compile();
+    service = app.get<AuthService>(AuthService);
 
-    service = module.get<AuthService>(AuthService);
+    userModel = app.get<Model<User>>(getModelToken(User.name));
+
+    mongoc = new MongoClient(uri);
+    await mongoc.connect();
+  });
+
+  beforeEach(async () => {
+    await mongoc.db('test').collection('users').deleteMany({});
+    user1 = new userModel({
+      username: 'test1',
+      email: 'test@email.com',
+      password: 'password',
+      dateCreated: new Date(),
+    });
+
+    await user1.save();
+  });
+
+  afterAll(async () => {
+    await mongoc.close();
+    await disconnect();
+    await mongod.stop();
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should register a user', async () => {
+    const user = await service.register({
+      username: 'John Doe',
+      email: 'mock@email.com',
+      password: 'password',
+    });
+    expect(user).toBeDefined();
   });
 });
